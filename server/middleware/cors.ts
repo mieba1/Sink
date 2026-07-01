ddb5e28eaff1c16c5309b84a899ef9a218c9cb94
mieba1 <104483725+mieba1@users.noreply.github.com>
2026-07-01 05:27:20 -0700

    cors跨域

---
+++ server/middleware/cors.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler((event) => {
+  setHeader(event, 'Access-Control-Allow-Origin', '*')
+  setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  if (event.method === 'OPTIONS') {
+    return ''
+  }
+})
