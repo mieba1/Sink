17169abae821c95e13503fbc24a1af093d59d228
mieba1 <104483725+mieba1@users.noreply.github.com>
2026-07-01 06:12:31 -0700

    Delete server/middleware/cors.ts

--- server/middleware/cors.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  setHeader(event, 'Access-Control-Allow-Origin', 'https://p-1317230264.cos.ap-guangzhou.myqcloud.com')
-  setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization')
-
-  if (event.method === 'OPTIONS') {
-    return ''
-  }
-})
