4bf97c35e5a86e5a9a989798299a2cba7ef10ca6
mieba1 <104483725+mieba1@users.noreply.github.com>
2026-07-01 05:32:06 -0700

    Update cors.ts

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler((event) => {
-  setHeader(event, 'Access-Control-Allow-Origin', '*')
+  setHeader(event, 'Access-Control-Allow-Origin', 'https://p-1317230264.cos.ap-guangzhou.myqcloud.com')
   setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
